00ccd825f029b43ac36b04423accf388c2d80826
Chana Kupetz <chanakupetz@gmail.com>
2020-07-27 01:41:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,11 +7,12 @@ end
 def my_hash
 {  name: "chana"
   }
-  # return a valid hash with any key/value pair of your choice
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {
+    :name => 'Grace Hopper'
+  }
 end
 
 
